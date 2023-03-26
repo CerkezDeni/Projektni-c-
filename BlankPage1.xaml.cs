8a87c76a854327e34e1284ca75c79f7415b06d7a
PerAsperaAdAstrq <105001829+PerAsperaAdAstrq@users.noreply.github.com>
2023-03-26 19:17:01 +0200

    nig

--- BlankPage1.xaml.cs
+++ BlankPage1.xaml.cs
@@ -12,19 +12,88 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.System.Threading;
 
 namespace Projektni
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class BlankPage1 : Page
     {
+        private CancellationTokenSource cancellationTokenSource;
+
         public BlankPage1()
         {
             this.InitializeComponent();
+            cancellationTokenSource = new CancellationTokenSource();
+            CreateBackgroundThread();
+        }
+
+        private void timer_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private async void CreateBackgroundThread()
+        {
+            int sec = 0;
+            int min = 0;
+            string csec = "err";
+            string dmin = "err";
+            bool e = false;
+            bool destroy= false;
+            await Task.Run(async () =>
+            {
+                
+                while (!cancellationTokenSource.IsCancellationRequested || destroy==false)
+                {
+                    if(min==60)
+                    {
+                        timeraaa.Text = "EXPIRED";
+                        destroy= true;
+                    }
+                    if (e == true)
+                    {
+                        e = false;
+                        min++;
+                        sec = -1;
+                    }
+                    sec++;
+                    if (sec == 59)
+                    {
+                        e = true;
+                    }
+                    csec=sec.ToString();
+                    dmin=min.ToString();
+                    await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+                    {
+                        if(sec<10 && min>9)
+                        {
+                            timeraaa.Text = dmin + ":" + "0" + csec;
+                        }
+                        else if (sec > 9 && min < 10)
+                        {
+                            timeraaa.Text = "0" + dmin + ":" + csec;
+                        }
+                        else if (sec < 10 && min < 10)
+                        {
+                            timeraaa.Text = "0" + dmin + ":" + "0" + csec;
+                        }
+                        else if(sec > 9 && min > 9)
+                        {
+                            timeraaa.Text = dmin + ":" + csec;
+                        }
+                    });
+                    await Task.Delay(1000);
+                }
+            }, cancellationTokenSource.Token);
+        }
+
+        protected override void OnNavigatingFrom(NavigatingCancelEventArgs e)
+        {
+            cancellationTokenSource.Cancel();
+            base.OnNavigatingFrom(e);
         }
     }
 }
+

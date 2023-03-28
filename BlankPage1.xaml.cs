a39b6f0b0a6aae1e914ff8fa8811007f9fcce064
PerAsperaAdAstrq <105001829+PerAsperaAdAstrq@users.noreply.github.com>
2023-03-28 20:16:26 +0200

    Update

--- BlankPage1.xaml.cs
+++ BlankPage1.xaml.cs
@@ -23,6 +23,7 @@ namespace Projektni
     {
         bool endgame = false;
         List<int> randomNumbers;
+        int numbah;
         public BlankPage1()
         {
             this.InitializeComponent();
@@ -30,6 +31,7 @@ namespace Projektni
             SetImageForAllButtons();
             randomNumbers = GenerateRandomNumbers();
             Spawnajmine();
+            SweeperStatus();
         }
 
         public List<int> GenerateRandomNumbers()
@@ -97,6 +99,93 @@ namespace Projektni
 
         }
 
+        private void SweeperStatus()
+        {
+            string stringy;
+            int giganigga = 0;
+            for (int i = 1; i <= 256; i++)
+            {
+                string buttonName3 = "Gumb" + i;
+                Button gumb66 = FindName(buttonName3) as Button;
+                if (gumb66.Tag == "flag" || gumb66.Tag == "flagandbombara")
+                {
+                    giganigga++;
+                }
+            }
+            numbah = 40 - giganigga;
+            flagnumberdisplay.Text = numbah.ToString();
+            switch (giganigga)
+            {
+                case 0:
+                    string imagePath = "ms-appx:///Assets/smiley.png";
+                    ImageBrush imageBrush = new ImageBrush();
+                    imageBrush.ImageSource = new BitmapImage(new Uri(imagePath));
+                    Status.Background = imageBrush;
+                    break;
+                case 1:
+                    string imagePath1 = "ms-appx:///Assets/smiley1.png";
+                    ImageBrush imageBrush1 = new ImageBrush();
+                    imageBrush1.ImageSource = new BitmapImage(new Uri(imagePath1));
+                    Status.Background = imageBrush1;
+                    break;
+                case 2:
+                    string imagePath2 = "ms-appx:///Assets/smiley2.png";
+                    ImageBrush imageBrush2 = new ImageBrush();
+                    imageBrush2.ImageSource = new BitmapImage(new Uri(imagePath2));
+                    Status.Background = imageBrush2;
+                    break;
+                case 3:
+                    string imagePath3 = "ms-appx:///Assets/smiley3.png";
+                    ImageBrush imageBrush3 = new ImageBrush();
+                    imageBrush3.ImageSource = new BitmapImage(new Uri(imagePath3));
+                    Status.Background = imageBrush3;
+                    break;
+                case 4:
+                    string imagePath4 = "ms-appx:///Assets/smiley4.png";
+                    ImageBrush imageBrush4 = new ImageBrush();
+                    imageBrush4.ImageSource = new BitmapImage(new Uri(imagePath4));
+                    Status.Background = imageBrush4;
+                    break;
+                case 5:
+                    string imagePath5 = "ms-appx:///Assets/smiley5.png";
+                    ImageBrush imageBrush5 = new ImageBrush();
+                    imageBrush5.ImageSource = new BitmapImage(new Uri(imagePath5));
+                    Status.Background = imageBrush5;
+                    break;
+                case 6:
+                    string imagePath6 = "ms-appx:///Assets/smiley6.png";
+                    ImageBrush imageBrush6 = new ImageBrush();
+                    imageBrush6.ImageSource = new BitmapImage(new Uri(imagePath6));
+                    Status.Background = imageBrush6;
+                    break;
+                case 7:
+                    string imagePath7 = "ms-appx:///Assets/smiley7.png";
+                    ImageBrush imageBrush7 = new ImageBrush();
+                    imageBrush7.ImageSource = new BitmapImage(new Uri(imagePath7));
+                    Status.Background = imageBrush7;
+                    break;
+                case 8:
+                    string imagePath8 = "ms-appx:///Assets/smiley8.png";
+                    ImageBrush imageBrush8 = new ImageBrush();
+                    imageBrush8.ImageSource = new BitmapImage(new Uri(imagePath8));
+                    Status.Background = imageBrush8;
+                    break;
+                case 9:
+                    string imagePath9 = "ms-appx:///Assets/smiley9.png";
+                    ImageBrush imageBrush9 = new ImageBrush();
+                    imageBrush9.ImageSource = new BitmapImage(new Uri(imagePath9));
+                    Status.Background = imageBrush9;
+                    break;
+               default:
+                    string imagePath10 = "ms-appx:///Assets/smiley9.png";
+                    ImageBrush imageBrush10 = new ImageBrush();
+                    imageBrush10.ImageSource = new BitmapImage(new Uri(imagePath10));
+                    Status.Background = imageBrush10;
+                    break;
+            }
+
+
+        }
         private async void CreateBackgroundThread()
         {
             int sec = 0;
@@ -166,25 +255,31 @@ namespace Projektni
                     {
                         if (pressedButton.Tag != "clicked")
                         {
+                            
                             if (pressedButton.Tag != "flag" && pressedButton.Tag != "flagandbombara")
                             {
-                                if (pressedButton.Tag == "bombara")
+                                if(numbah > 0)
                                 {
-                                    pressedButton.Tag = "flagandbombara";
+                                    if (pressedButton.Tag == "bombara")
+                                    {
+                                        pressedButton.Tag = "flagandbombara";
 
-                                    string imagePath = "ms-appx:///Assets/flagico.png";
-                                    ImageBrush imageBrush = new ImageBrush();
-                                    imageBrush.ImageSource = new BitmapImage(new Uri(imagePath));
-                                    pressedButton.Background = imageBrush;
-                                }
-                                else
-                                {
-                                    string imagePath = "ms-appx:///Assets/flagico.png";
-                                    ImageBrush imageBrush = new ImageBrush();
-                                    imageBrush.ImageSource = new BitmapImage(new Uri(imagePath));
-                                    pressedButton.Background = imageBrush;
+                                        string imagePath = "ms-appx:///Assets/flagico.png";
+                                        ImageBrush imageBrush = new ImageBrush();
+                                        imageBrush.ImageSource = new BitmapImage(new Uri(imagePath));
+                                        pressedButton.Background = imageBrush;
+                                        SweeperStatus();
+                                    }
+                                    else if (pressedButton.Tag == "clear")
+                                    {
+                                        string imagePath = "ms-appx:///Assets/flagico.png";
+                                        ImageBrush imageBrush = new ImageBrush();
+                                        imageBrush.ImageSource = new BitmapImage(new Uri(imagePath));
+                                        pressedButton.Background = imageBrush;
 
-                                    pressedButton.Tag = "flag";
+                                        pressedButton.Tag = "flag";
+                                        SweeperStatus();
+                                    }
                                 }
                             }
                             else
@@ -197,6 +292,7 @@ namespace Projektni
                                     ImageBrush imageBrush = new ImageBrush();
                                     imageBrush.ImageSource = new BitmapImage(new Uri(imagePath));
                                     pressedButton.Background = imageBrush;
+                                    SweeperStatus();
                                 }
                                 else
                                 {
@@ -206,6 +302,7 @@ namespace Projektni
                                     pressedButton.Background = imageBrush;
 
                                     pressedButton.Tag = "clear";
+                                    SweeperStatus();
                                 }
                             }
                         }
@@ -220,16 +317,39 @@ namespace Projektni
             Button pressedButton = sender as Button;
             if(endgame == false)
             {
-                if (pressedButton.Tag != "clicked" && pressedButton.Tag != "flag")
+                if (pressedButton.Tag != "clicked" && pressedButton.Tag != "flag" && pressedButton.Tag != "flagandbombara")
                 {
                     if (pressedButton.Tag == "bombara")
                     {
+                        for (int i = 1; i <= 256; i++)
+                        {
+                            string buttonName3 = "Gumb" + i;
+                            Button gumb69 = FindName(buttonName3) as Button;
+                            if (gumb69 != null)
+                            {
+                                if (gumb69.Tag == "bombara" || gumb69.Tag == "flagandbombara")
+                                {
+                                    string imagePath = "ms-appx:///Assets/tilebomb.png";
+                                    ImageBrush imageBrush = new ImageBrush();
+                                    imageBrush.ImageSource = new BitmapImage(new Uri(imagePath));
+                                    gumb69.Background = imageBrush;
+                                }
+                            }
+                        }
+                        string imagePathnig = "ms-appx:///Assets/tilebombexploded.png";
+                        ImageBrush imageBrushnig = new ImageBrush();
+                        imageBrushnig.ImageSource = new BitmapImage(new Uri(imagePathnig));
+                        pressedButton.Background = imageBrushnig;
                         endgame = true;
                         //dovrsi kasnije
                     }
                     else
                     {
                         pressedButton.Tag = "clicked";
+                        string imagePath = "ms-appx:///Assets/tileclicked.png";
+                        ImageBrush imageBrush = new ImageBrush();
+                        imageBrush.ImageSource = new BitmapImage(new Uri(imagePath));
+                        pressedButton.Background = imageBrush;
                     }
                 }
             }
